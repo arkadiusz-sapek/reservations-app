94983a4ae16b9c74796577e80a803b5e08bd1aba
Arkadiusz Sapek <a.sapek@clearcode.cc>
2021-07-25 21:27:32 +0200

    Improve styles for mobile

--- src/features/reservations/components/TimeSlotCard/timeSlotsStyles.ts
+++ src/features/reservations/components/TimeSlotCard/timeSlotsStyles.ts
@@ -24,17 +24,22 @@ export const TimeSlotWrapper = styled.div<TimeSlotWrapperProps>`
     display: flex;
     justify-content: center;
     align-items: center;
-
-    padding: 0.5rem;
-    margin: 0.25rem;
-    width: 6.5rem;
-    height: 2rem;
+    padding: 0.2rem;
+    margin: 0.1rem;
+    width: 6rem;
+    height: 1.5rem;
     box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%),
         0px 1px 3px 0px rgb(0 0 0 / 12%);
     background-color: ${({ backgroundColor }) => backgroundColor};
     color: ${({ textColor }) => textColor};
-
     cursor: ${({ cursor }) => cursor};
+
+    ${({ theme }) => theme.breakpoints.xl} {
+        padding: 0.5rem;
+        margin: 0.25rem;
+        width: 6.5rem;
+        height: 2rem;
+    }
 `;
 
 export const TimeDesignation = styled.span`
