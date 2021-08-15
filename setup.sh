b6db4c25262bddfff17ab6457d5eb526ef13d46c
Arkadiusz Sapek <a.sapek@clearcode.cc>
2021-08-15 20:58:13 +0200

    Change configs files

--- setup.sh
+++ setup.sh
@@ -7,4 +7,3 @@ sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo service docker start
-sudo make install
